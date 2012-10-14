a1e02b16454d44878027ea2ac58defb54e201deb
Kim Persson <forgetmenotox@notingham.se>
2012-10-14 16:30:04 +0200

    WIP Fetch sun times.

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ require "yahoo_weatherman"
 ARUBA_WOEID = 23424736
 DEPARTURE_DATE = Time.new(2012, 12, 18, 0, 0, 0, "+01:00")
 SECONDS_PER_DAY = 86400
+ARUBA_TIMEZONE_OFFSET = 4 * 3600
 
 get '/' do
   calculate_days_to_departure
@@ -35,9 +36,24 @@ end
 def fetch_current_weather_info
   client = Weatherman::Client.new
   weather = client.lookup_by_woeid ARUBA_WOEID
+
   @image = weather.description_image.values.first
   @forecast_low = weather.forecasts.first['low']
   @forecast_high = weather.forecasts.first['high']
   @current_temperature = weather.condition['temp']
   @current_condition = weather.condition['text']
+
+  fetch_sun_times(weather)
+end
+
+def fetch_sun_times(weather)
+  sun_info = weather.astronomy.map {|e| e}
+  local_sunrise = sun_info.first.last
+  local_sunset = sun_info.last.last
+  @sunrise = local_aruba_to_utc(local_sunrise)
+  @sunset = local_aruba_to_utc(local_sunset)
+end
+
+def local_aruba_to_utc(time)
+  Time.parse("#{time} UTC") + ARUBA_TIMEZONE_OFFSET
 end
